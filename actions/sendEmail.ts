8e382a1caeb4d08041e5946980a85bae2cd4d470
infiniteltd <chrissamps24@gmail.com>
2024-02-14 13:31:45 +0100

    new commit

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -27,7 +27,7 @@ export const sendEmail = async (formData: FormData) => {
   let data;
   try {
     data = await resend.emails.send({
-      from: "Contact From <onboarding@resend.dev>",
+      from: "Contact Form <onboarding@resend.dev>",
       to: "chrissamps24@gmail.com",
       subject: "Message from contact form",
       reply_to: senderEmail as string,

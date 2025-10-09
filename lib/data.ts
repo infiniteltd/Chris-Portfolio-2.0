f672913f7bc5c46d9bac437ce32be4f57180615b
Christian <purposec60@gmail.com>
2025-10-09 09:43:23 +0100

    removed one tech stack to avoid redundancy

--- lib/data.ts
+++ lib/data.ts
@@ -133,7 +133,7 @@ export const projectsData = [
     title: "Goal Genius",
     description:
       "An interactive football quiz built with modern Tech stacks, featuring timed questions, instant feedback, and summary to challenge and engage users",
-    tags: ["Next.js", "React", "TypeScript", "Tailwind CSS"],
+    tags: ["Next.js", "TypeScript", "Tailwind CSS"],
     imageUrl: goalImg,
     github: "https://github.com/infiniteltd/goal-genius",
     live: "https://goal-genius.vercel.app/",

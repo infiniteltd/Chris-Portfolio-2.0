d9e42db269d6bb39c5c418385803fb9cbc90fb99
Christian <purposec60@gmail.com>
2025-11-07 08:41:26 +0100

    updated youtube channel link in intro.tsx

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -79,7 +79,7 @@ export default function Intro() {
           href="#contact"
           className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
           onClick={() => {
-            setActiveSection("Contact"), setTimeOfLastClick(Date.now());
+            (setActiveSection("Contact"), setTimeOfLastClick(Date.now()));
           }}
         >
           Contact me here{" "}
@@ -119,7 +119,7 @@ export default function Intro() {
         </a>
 
         <a
-          href="https://www.youtube.com/channel/UC7V9PH1vEBkhCrbqCNwdFyQ"
+          href="https://www.youtube.com/@NLHustle"
           target="_blank"
           className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
         >
